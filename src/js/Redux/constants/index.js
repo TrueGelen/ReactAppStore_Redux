c2dd8d51493ea2dcb3e1e1bbcde6c5bf99ab3491
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-10-20 14:54:32 +0300

    fixed url for github page

--- src/js/Redux/constants/index.js
+++ src/js/Redux/constants/index.js
@@ -1,10 +1,10 @@
-// export const baseUrlAssets = 'ReactAppStore/dist/'
-export const baseUrlAssets = '/'
+export const baseUrlAssets = 'ReactAppStore/dist/'
+// export const baseUrlAssets = '/'
 //dev base url
 export const baseUrlImgs = {
-	phones: `${baseUrlAssets}assets/imgs/phones/`,
-	televisions: `${baseUrlAssets}assets/imgs/televisions/`,
-	tablets: `${baseUrlAssets}assets/imgs/tablets/`
+  phones: `${baseUrlAssets}assets/imgs/phones/`,
+  televisions: `${baseUrlAssets}assets/imgs/televisions/`,
+  tablets: `${baseUrlAssets}assets/imgs/tablets/`
 }
 
 //dist base url. for gitHub page
@@ -15,28 +15,28 @@ export const baseUrlImgs = {
 } */
 
 export const productLabelsForStores = {
-	television: {
-		diagonal: 'Диагональ',
-		hz: 'Частота обновления экрана (Гц)',
-		screenResolution: 'Разрешение экрана',
-		wifi: 'Wi-Fi',
-		about: 'О товаре'
-	},
-	phones: {
-		diagonal: "Диагональ",
-		frontCamera: "Фронтальная камера (МП)",
-		mainCamera: "Основная камера (МП)",
-		processor: "Процессор",
-		memory: "Объем памяти (Гб)",
-		about: 'О товаре'
-	},
-	tablets: {
-		diagonal: "Диагональ",
-		frontCamera: "Фронтальная камера (МП)",
-		mainCamera: "Основная камера (МП)",
-		processor: "Процессор",
-		memory: "Объем памяти (Гб)",
-		about: 'О товаре'
-	}
+  television: {
+    diagonal: 'Диагональ',
+    hz: 'Частота обновления экрана (Гц)',
+    screenResolution: 'Разрешение экрана',
+    wifi: 'Wi-Fi',
+    about: 'О товаре'
+  },
+  phones: {
+    diagonal: "Диагональ",
+    frontCamera: "Фронтальная камера (МП)",
+    mainCamera: "Основная камера (МП)",
+    processor: "Процессор",
+    memory: "Объем памяти (Гб)",
+    about: 'О товаре'
+  },
+  tablets: {
+    diagonal: "Диагональ",
+    frontCamera: "Фронтальная камера (МП)",
+    mainCamera: "Основная камера (МП)",
+    processor: "Процессор",
+    memory: "Объем памяти (Гб)",
+    about: 'О товаре'
+  }
 }
 
